556628c11eadcb7713c9b9387dbf1365b87c4168
hafizip00 <hafizip00@gmail.com>
2024-01-06 11:14:32 +0500

    Footer Fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div class="group relative">
-  <div class="box bg-gray-300 p-10 transition duration-300 ease-in-out hover:bg-blue-500">
-    Hover me
-  </div>
-  <div class="hidden group-hover:block absolute top-0 left-0 right-0 bottom-0 bg-blue-500 opacity-75"></div>
-  <button class="hidden group-hover:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white text-blue-500 px-4 py-2 rounded-md">
-    Click me
-  </button>
+    <div class="w-full bg-blue-700 group h-[200px]">
+      <div className='relative transition-all w-full overflow-hidden'>
+          <div className='flex justify-center items-center transform transition duration-500 ease-in-out bg-red-500 w-full h-[200px] group-hover:translate-y-0 translate-y-32 group-hover:opacity-100 opacity-0'>
+              <button className='p-2 bg-green-600'>Hit Me</button>
+          </div>
+      </div>
 </div>
   )
 }
